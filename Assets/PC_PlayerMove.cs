1139c4601bfe073faefa9691eeabd2e3f682ff81
narishiho0804 <56993978+narishiho0804@users.noreply.github.com>
2025-05-06 01:53:22 +0900

    Animationを前後左右のアニメーションの設定をやった。Scriptも書いた。

--- Assets/PC_PlayerMove.cs
+++ Assets/PC_PlayerMove.cs
@@ -1,34 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Types;
 using UnityEngine;
 
 public class PC_PlayerMove : MonoBehaviour
 {
     private Rigidbody rb;
-    private float speed = 3.0f;
+    private Animator animator;
+    private float speed = 15.0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))// Wキーを押す。
         {
-            rb.velocity = transform.forward * speed;
+            animator.SetBool("Walk", true);
+            
+            rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.velocity = - transform.forward * speed;
+            animator.SetBool("Back", true);
+            rb.AddForce(transform.forward * speed * -1, ForceMode.Acceleration);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = transform.right * speed;
+            animator.SetBool("WalkRight", true);
+            rb.AddForce(transform.right * speed, ForceMode.Acceleration);
+            
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = -transform.right * speed;
+            animator.SetBool("WalkLeft", true);
+            rb.AddForce(transform.right * speed * -1, ForceMode.Acceleration);
+        }
+
+
+
+
+        if (Input.GetKeyUp(KeyCode.W))// Wキーを押す。
+        {
+            animator.SetBool("Walk", false);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Back", false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("WalkRight", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("WalkLeft", false);
         }
     }
 }
